d73e2138e6d13f269b785016edffab7513d930bf
Mehdi Haned <66488794+Mehdi-Haned@users.noreply.github.com>
2022-06-20 18:56:23 -0400

    Update ItemListElement.js

--- src/components/ItemList/ItemListElement.js
+++ src/components/ItemList/ItemListElement.js
@@ -2,20 +2,50 @@ import { useState } from "react"
 
 function ItemListElement({ item, cost, setCost }) {
     
+    const [ itemDetails, setItemDetails ] = useState(null)
+    const [ visible, setVisible ] = useState(true)
+    const price = item.cost
+    
     const HandleCheck = (event) => {
-
-        if (event.target.checked) {
-            setCost(cost + parseFloat(event.target.value));
+        const extraPrice = item.cost
+        
+        if (item.stock <= 0) {
+            event.target.checked = false
+            alert("Sorry, we are out of stock");
+        } else if (event.target.checked) {
+            setCost(cost + parseFloat(extraPrice));
         } else {
-            setCost(cost - parseFloat(event.target.value));
+            setCost(cost - parseFloat(extraPrice));
         }
+    };
+
+    const HandleClick = (event) => {
+        if (visible) {
+            fetch(`https://we-accelerate-group199-backend.herokuapp.com/api/v1/items/${item.id}`)
+            .then(res => res.json())
+            .then(res => setItemDetails(res))
+            setVisible(false)
+        } else {
+            setVisible(true)
+            setItemDetails(null)
+        }
+    
     };
 
     return(
         <>
             <div>
                 <input type="checkbox" value={item.cost} onChange={HandleCheck}/>
-                {item.name} &nbsp; - &nbsp; {item.cost}$
+                {item.name} &nbsp; - &nbsp; {price}$ &nbsp; &nbsp;
+                <button onClick={HandleClick}>More details</button>
+                {itemDetails  && (
+                    <div>
+                        <h3>Details:</h3>
+                        <div>Price: {itemDetails.cost}$</div>
+                        <div>Brand: {itemDetails.brand}</div>
+                        <div>Number in stock: {itemDetails.stock}</div>
+                    </div>
+                )}
             </div>
         </>
     )
