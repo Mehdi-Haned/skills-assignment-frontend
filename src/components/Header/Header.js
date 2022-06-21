a9df929621eb36b8f96b758c93ea906cf810a43b
Mehdi Haned <66488794+Mehdi-Haned@users.noreply.github.com>
2022-06-20 23:41:57 -0400

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,7 @@ import './Header.css';
 export default function Header() {
   return (
     <div className="Header">
-        <h1>Cobalt Hardware</h1>
+        <h1>Cobalt Hardware</h1><h4>A WE accellerate creation</h4>
     </div>
   );
 }
